0ca17b528a76d59cd8d6d14ec5a8310137bb9780
bearbig-12 <bearbig23@gmail.com>
2022-03-28 16:53:29 +0900

    Class

---
+++ CLASS/ClassChapter1/main.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+
+//	C++ 클래스
+
+//		1. 객체(Object) : 클래스로 만들어진 하나의 인스턴스(instance)
+
+//		2. 클래스(Class)
+//			
+//			상태(state)
+//				멤버변수(member variable) - C++
+//				필드(Field)				  - C#, JAVA
+
+//			행동(Behaviour)
+//				멤버함수(Member Function) - C++
+//				메써드(Method)			  - C#, JAVA
+
+
+//		3. 캡슐화(Encapsulation)
+// 
+//				불필요한 정보를 숨겨서 쉽게 사용
+//				Information Hiding
+//					- 불필요한 정보를 숨겨서 쉽게 사용
+//					- 보안 : 괜히 건드려서 좋을 것 없는 정보 숨기기
+
+
+//		4. 상속(Inheritance), 파생(Derivation)
+
+//			상속 - 부모(parent) 클래스 , 자식(Child) 클래스
+//			파생 - 기반(base) 클래스 , 파생(Derived) 클래스
+
+
+//		5. 다형성(Polymorphism)
+
+//			나초는 고양이입니다.
+//			나초는 동물입니다.
+
+//----------------------------------------------------------------------
+
+//		생성자(constructor) , 소멸자(Destructor)
+//			객체가 만들어질때 자동적으로 생성자가 사용, 소멸될때 사용자가 사용
+
+//		Static Member
+//			정적 멤버: 인스턴스와 상관없이 하나만 존재하는 멤버
+
+//		Abstract Class
+//			개념만으로 존재하는 클래스
+//			Interface
+//				UI - User Interface
+//				HDMI
+//				Audio Interface
+//				USB
+
+int main()
+{
+
+}
