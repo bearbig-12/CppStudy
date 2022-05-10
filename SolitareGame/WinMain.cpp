56531232d708627df39946b6fdf0accdb8fc0d37
bearbig-12 <bearbig23@gmail.com>
2022-05-10 16:59:23 +0900

    SolitareGame

---
+++ SolitareGame/WinMain.cpp
@@ -0,0 +1,107 @@
+#include <Windows.h>
+#include <gdiplus.h>
+
+#pragma comment (lib, "Gdiplus.lib")
+
+const wchar_t gClassName[] = L"MyWindowClass";
+
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam);
+
+int WINAPI WinMain(
+	_In_ HINSTANCE hInstance,
+	_In_opt_ HINSTANCE hPrevInstance,
+	_In_ LPSTR lpCmdLine,
+	_In_ int nShowCmd
+)
+{
+	Gdiplus::GdiplusStartupInput gdiplusStartupInput;
+	ULONG_PTR gdiplusToken;
+	Gdiplus::GdiplusStartup(&gdiplusToken, &gdiplusStartupInput, nullptr);
+
+	HWND hwnd;
+	WNDCLASSEX wc;
+
+	ZeroMemory(&wc, sizeof(WNDCLASSEX));
+
+	wc.style = CS_HREDRAW | CS_VREDRAW;
+	wc.lpszClassName = gClassName;
+	wc.hInstance = hInstance;
+	wc.hCursor = LoadCursor(NULL, IDC_ARROW);
+	wc.hbrBackground = (HBRUSH)COLOR_WINDOW;
+	wc.lpfnWndProc = WindowProc;
+	wc.cbSize = sizeof(WNDCLASSEX);
+
+	if (!RegisterClassEx(&wc))
+	{
+		MessageBox(nullptr, L"Failed to register Window Class!", L"Error", MB_OK);
+		return 0;
+	}
+
+	RECT wr = { 0,0,1024,768 };
+	AdjustWindowRect(&wr, WS_OVERLAPPEDWINDOW, FALSE);
+	hwnd = CreateWindowEx(
+		NULL,
+		gClassName,
+		L"SolitareGame",
+		WS_OVERLAPPEDWINDOW,
+		CW_USEDEFAULT,
+		CW_USEDEFAULT,
+		wr.right - wr.left,
+		wr.bottom - wr.top,
+		NULL,
+		NULL,
+		hInstance, 
+		NULL
+	);
+	if (!hwnd)
+	{
+		MessageBox(nullptr, L"Failed to Create Window!", L"Error", MB_OK);
+		return 0;
+	}
+	ShowWindow(hwnd, nShowCmd);
+	UpdateWindow(hwnd);
+
+	MSG msg;
+	while (GetMessage(&msg, NULL, 0, 0))
+	{
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+
+	Gdiplus::GdiplusShutdown(gdiplusToken);
+	return static_cast<int>(msg.wParam);
+}
+
+void OnPaint(HWND hwnd)
+{
+	HDC hdc;
+	PAINTSTRUCT ps;
+	
+	hdc = BeginPaint(hwnd, &ps);
+
+	Gdiplus::Graphics graphics(hdc);
+
+	EndPaint(hwnd, &ps);
+}
+
+
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+	switch (message)
+	{
+	case WM_PAINT:
+		OnPaint(hwnd);
+		break;
+	case WM_CLOSE:
+		DestroyWindow(hwnd);
+		break;
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		break;
+	default:
+		return DefWindowProc(hwnd, message, wParam, lParam);
+	}
+
+	return 0;
+}
+
